9c0e5ce1bb7b2bfd96198c0dd854d7879a5ff843
Arkadiusz Lubowicki <s106960@ap.be>
2021-02-10 17:39:27 +0100

    Labo 1 gedaan

---
+++ Labo1/printNameAndAge.ts
@@ -0,0 +1,5 @@
+let voornaam: string = `Arkadiusz`;
+let achternaam: string = `Lubowicki`;
+let leeftijd: string = `23`;
+let zin: string = `Ik ben ${voornaam} ${achternaam} en ik ben ${leeftijd} jaar oud.`;
+console.log(zin);
