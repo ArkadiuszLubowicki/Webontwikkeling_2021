4c806e813c9773d94c85febcac11e28bea9a2534
Arkadiusz Lubowicki <s106960@ap.be>
2021-02-24 17:32:07 +0100

    Labo2 oefening2 met while loop omgekeerde volgorde van regenboog geprint(oefening 2 klaar)

--- Labo2/oefening2.ts
+++ Labo2/oefening2.ts
@@ -15,3 +15,13 @@ for(let i = 0; i < 7; i++)
     regenboog.push(Kleur[i]);
     console.log(Kleur[i]);
 }
+
+console.log('Omgekeerde volgorde.');
+
+let kleur: string;
+
+while(regenboog.length != 0)
+{
+    kleur = regenboog.pop();
+    console.log(kleur);
+}
