d98c04b96e4ef65613788b1605e68dff0e2adea8
Arkadiusz Lubowicki <s106960@ap.be>
2021-02-24 17:02:17 +0100

    Labo2 oefening1 array tonen met while loop

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -1,11 +1,17 @@
 let getallen: number[] = [];
 let getal: number = 100;
 
+
 for (let i = 0; i <= 100; i++)
 {
     getallen[i] = getal;
-    console.log(getal);
     getal++;
-
 }
-console.log(getallen);
+
+let teller: number = 0;
+
+while(teller<=100)
+{
+    console.log(getallen[teller]);
+    teller++;
+}
