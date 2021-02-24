9a9cad4ff38f61e70626c28a0d172a7b66110bcd
Arkadiusz Lubowicki <s106960@ap.be>
2021-02-24 16:55:00 +0100

    Labo2 oefening1 array ingevuld met getallen van 100 te 200 met for-lus

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -1,1 +1,11 @@
 let getallen: number[] = [];
+let getal: number = 100;
+
+for (let i = 0; i <= 100; i++)
+{
+    getallen[i] = getal;
+    console.log(getal);
+    getal++;
+
+}
+console.log(getallen);
