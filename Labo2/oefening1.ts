ce9eeec36a9a83dfda6f5e104127a08ad989df86
Arkadiusz Lubowicki <s106960@ap.be>
2021-02-24 17:10:03 +0100

    Labo2 oefening1 een for loop om getallen te tonen die deelbaar door 3 zijn(oefening1 klaar)

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -2,7 +2,7 @@ let getallen: number[] = [];
 let getal: number = 100;
 
 
-for (let i = 0; i <= 100; i++)
+for(let i = 0; i <= 100; i++)
 {
     getallen[i] = getal;
     getal++;
@@ -14,4 +14,14 @@ while(teller<=100)
 {
     console.log(getallen[teller]);
     teller++;
+}
+
+console.log('Nu enkel getallen die deelbaar zijn door 3.')
+
+for(let i = 0; i <= 100; i++)
+{
+    if(getallen[i] % 3 == 0)
+    {
+        console.log(getallen[i]);
+    }
 }
