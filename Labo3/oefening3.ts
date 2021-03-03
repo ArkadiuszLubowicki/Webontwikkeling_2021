38cd65b918720e2ead498fd6f101d20174087694
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-03 17:21:45 +0100

    "Oefening 3" volledig af

--- Labo3/oefening3.ts
+++ Labo3/oefening3.ts
@@ -1,11 +1,16 @@
-function multiplyText(amount, text){
+function multiplyText(amount: number, text: string = "default", appendix?: string): string{
     let tekst: string = "";
     for(let i = 0; i < amount; i++){
         tekst = tekst.concat(text + " ");
     }
+
+    if(appendix != undefined){
+        tekst = tekst.concat(appendix);
+    }
+
     return tekst;
 }
 
-let result: string = multiplyText(3, "flower");
+let result: string = multiplyText(3, "Arkadiusz Lubowicki", "?");
 
 console.log(result);
