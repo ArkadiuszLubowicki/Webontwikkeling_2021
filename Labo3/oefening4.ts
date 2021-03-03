8a9dfdfaef4e2c968752fdcde7ef37373541179d
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-03 17:56:55 +0100

    "oefening 4" gemaakt

---
+++ Labo3/oefening4.ts
@@ -0,0 +1,27 @@
+enum Color{
+    Red,
+    Green,
+    Blue,
+}
+
+
+function multiplyTextColor(amount: number, text: string = "default", color: Color , appendix?: string){
+    let tekst: string = "";
+    for(let i = 0; i < amount; i++){
+        tekst = tekst.concat(text + " ");
+    }
+
+    if(appendix != undefined){
+        tekst = tekst.concat(appendix);
+    }
+
+    console.log("%c"+tekst, "color: red");
+    //console.log(kleur(tekst));
+    return tekst;
+}
+
+multiplyTextColor(3, "Arkadiusz Lubowicki", Color.Red, "?")
+
+//let result_4: string = multiplyTextColor(3, "Arkadiusz Lubowicki", Color.Red, "?");
+
+//console.log(result_4);
