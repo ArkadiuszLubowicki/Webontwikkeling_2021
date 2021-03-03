cf80fbb9def7462815bbe6b719ed6fe040a1d18f
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-03 16:50:08 +0100

    "Oefening 1: Getallen zoeken" klaar

---
+++ Labo3/oefening1.ts
@@ -0,0 +1,8 @@
+let items = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87];
+
+function search(eenArray, teZoeken){
+    return eenArray.indexOf(teZoeken);
+}
+
+console.log(search(items, 5)); // 1 
+console.log(search(items, 15)); // 6
---
+++ Labo3/oefening2.ts
@@ -0,0 +1,7 @@
+let word: string = "sven is here";
+
+for(let c of word){​​
+
+    console.log(c);
+
+}​​
