ea5270fb5dd15bb39d3fda6f5aed4ebd4002f839
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-10 16:42:54 +0100

    Vergeten types te geven in de parameters van een functie

--- Labo3/oefening1.ts
+++ Labo3/oefening1.ts
@@ -1,6 +1,6 @@
 let items = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87];
 
-function search(eenArray, teZoeken){
+function search(eenArray: number[], teZoeken: number){
     return eenArray.indexOf(teZoeken);
 }
 
