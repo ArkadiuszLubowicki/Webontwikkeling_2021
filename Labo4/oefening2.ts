ad7645c5d2e802c5ab2750f5d62be10bde87e48b
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-10 16:31:23 +0100

    Labo 4 oefening 2 variabele "thematrix" aangemaakt

---
+++ Labo4/oefening2.ts
@@ -0,0 +1,9 @@
+interface Movie {
+    title: string,
+    year: number,
+    actors: string[],
+    metascore: number,
+    seen: boolean
+}
+
+let thematrix: Movie = {title: "The Matrix", year: 1999, actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"], metascore: 73, seen: true};
