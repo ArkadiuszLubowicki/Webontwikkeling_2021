12daa6f6ec0109213ba2a76ab5e83c5fa43d8dbb
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-10 16:40:42 +0100

    Labo 4 oefening2, myfavoritemovie en myworstmovie aangemaakt (oefening klaar)

--- Labo4/oefening2.ts
+++ Labo4/oefening2.ts
@@ -7,3 +7,8 @@ interface Movie {
 }
 
 let thematrix: Movie = {title: "The Matrix", year: 1999, actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"], metascore: 73, seen: true};
+
+let myfavoritemovie: Movie = {title: "Shrek", year: 2001, actors: ["Mike Myers", "Eddie Murphy", "Cameron Diaz"], metascore: 84, seen: true};
+
+let myworstmovie: Movie = {title: "The Last Airbender", year: 2010, actors: ["Noah Ringer", "Dev Patel", "Nicola Pelts", "Jackson Rathbone"], metascore: 20, seen: true};
+
