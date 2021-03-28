adf642709559e3cf27859f2dd2c14cac7e7a3fa2
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-28 17:50:18 +0200

    Labo 6 oefening 3 Sorteren, extra lukt niet (oefening klaar en heel labo 6)

--- Labo6/oefening3.ts
+++ Labo6/oefening3.ts
@@ -17,28 +17,37 @@ let starterPokemonGen2 : Pokemon[]  = [
 
 // Spread
 let starters: Pokemon[] = [...starterPokemonGen1, ...starterPokemonGen2];
-//console.log(starters);
+console.log(starters);
 
 // Map
 let names: string[] = starters.map(e=>e.name);
-//console.log(names);
+console.log(names);
 
 // Filter
 let weakPokemon: Pokemon[] = starters.filter(e=>e.xp<=40);
-//console.log(weakPokemon);
+console.log(weakPokemon);
 
 // Combineren (Map + Filter)
 let weakPokemonNames: string[] = starters.filter(e=>e.xp<=40).map(e=>e.name);
-//console.log(weakPokemonNames);
+console.log(weakPokemonNames);
 
 // Reduce
 let sumOfAllXp = starters.reduce((sum, element) => sum + element.xp, 0);
-//console.log(sumOfAllXp);
+console.log(sumOfAllXp);
 
 // Ik weet niet hoe StrongestPokemon moet
 
 // Combineren (Reduce + Filter)
 let sumOfAllXpOfWeakPokemon = starters.filter(e=>e.xp<=40).reduce((sum, element) => sum + element.xp, 0);
 console.log(sumOfAllXpOfWeakPokemon)
+
+// Sorteren
+starters.sort((a, b) =>{
+    if(a.xp < b.xp) return -1;
+    if(a.xp > b.xp) return 1;
+    return 0;
+});
+console.log(starters);
+
 
 export {};
