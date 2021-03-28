5846a83fe271d92bb876ddf5252c03a827627cc3
Arkadiusz Lubowicki <s106960@ap.be>
2021-03-28 16:24:58 +0200

    Labo 6 oefening 1 evenMoreNumbers (oefening klaar)

--- Labo6/oefening1.ts
+++ Labo6/oefening1.ts
@@ -4,5 +4,9 @@ let numbers3: number[] = [7,8,9];
 
 let allNumbers: number[] = [...numbers1, ...numbers2, ...numbers3];
 console.log(allNumbers);
+
 let extraNumbers: number[] = [...allNumbers, 10, 11, 12];
 console.log(extraNumbers);
+
+let evenMoreNumbers: number[] = [-2, -1, 0, ...extraNumbers, 13, 14, 15];
+console.log(evenMoreNumbers);
