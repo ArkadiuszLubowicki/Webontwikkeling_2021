13927abdf323a6b99e7f83a58cf2c27e37d7a34a
Arkadiusz Lubowicki <s106960@ap.be>
2021-05-21 21:15:45 +0200

    let thisisme en whoamijson

--- Labo8/index.ts
+++ Labo8/index.ts
@@ -5,6 +5,11 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 
+let thisisme = {
+    name: "Sven",
+    age: 40,
+    profilePic: "https://www.1limburg.nl/sites/default/files/public/styles/media-paragraph/public/istock-619961796.jpg?itok=RuN0wixY"
+}
 
 app.get('/',(req:any,res:any)=>{
     res.render("index");
@@ -16,8 +21,7 @@ app.get('/whoami',(req:any,res:any)=>{
 });
 
 app.get('/whoamijson',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.json(thisisme);
 });
 
 app.get('/pikachujson',(req:any,res:any)=>{
