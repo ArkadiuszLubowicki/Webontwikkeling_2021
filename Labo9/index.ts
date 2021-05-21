76a7fec132cf2b044808cefe88c2c360971ea105
Arkadiusz Lubowicki <s106960@ap.be>
2021-05-21 21:55:03 +0200

    movies ejs pagina gemaakt

--- Labo9/index.ts
+++ Labo9/index.ts
@@ -6,8 +6,19 @@ app.set('view engine', 'ejs');
 app.set('port', 3000);
 app.use(express.static('public'));
 
+let movies = [
+    {name: "The Matrix", myScore: 90},
+    {name: "Pulp Fiction", myScore: 100},
+    {name: "Monster Hunter", myScore: 5},
+    {name: "Blade Runner", myScore: 100}
+];
+
 app.get('/',(req:any,res:any)=>{
     res.render("landing");
+});
+
+app.get('/movies',(req:any,res:any)=>{
+    res.render("movies", {movies:movies});
 });
 
 app.listen(app.get('port'), 
