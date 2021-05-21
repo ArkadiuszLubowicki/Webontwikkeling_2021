28ae47f1d440eb07b1a7db7fd782fe33b1235532
Arkadiusz Lubowicki <s106960@ap.be>
2021-05-21 20:29:33 +0200

    oefening 1.1

---
+++ Labo7/oefening1.ts
@@ -0,0 +1,17 @@
+const slowSum = (a: number, b: number) => {
+    return new Promise<number>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b);
+        },1000)
+    });
+}
+
+const slowMult = (a: number, b: number) => {
+    return new Promise<number>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a*b);
+        },1500)
+    });
+}
+
+slowSum(1,5).then(r=>console.log(r));
