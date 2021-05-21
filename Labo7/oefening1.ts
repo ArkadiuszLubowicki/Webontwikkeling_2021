09a92633e334859c2dd8f2bb48f0d378b70707bf
Arkadiusz Lubowicki <s106960@ap.be>
2021-05-21 20:33:08 +0200

    oefening 1.3

--- Labo7/oefening1.ts
+++ Labo7/oefening1.ts
@@ -14,5 +14,16 @@ const slowMult = (a: number, b: number) => {
     });
 }
 
+const slowDiv = (a: number, b: number) => {
+    return new Promise<number>((resolve, reject) => {
+        if(b ===0){
+            reject("You cannot divide by zero");
+        }
+        setTimeout(() => {
+            resolve(a/b);
+        },2000)
+    });
+}
+
 slowSum(1,5).then(r=>console.log(r));
 slowSum(1,5).then(r=>slowMult(r,2)).then(r=>console.log(r));
