7334215e2a40b168397e603dc5156cf3a3d0ecb0
Arkadiusz Lubowicki <s106960@ap.be>
2021-05-21 21:24:11 +0200

    pikachujson tonen

--- Labo8/index.ts
+++ Labo8/index.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const fetch = require('node-fetch');
 
 app.set('view engine', 'ejs');
 app.set('port', 3000);
@@ -10,6 +11,14 @@ let thisisme = {
     age: 24,
     profilePic: "https://www.1limburg.nl/sites/default/files/public/styles/media-paragraph/public/istock-619961796.jpg?itok=RuN0wixY"
 }
+
+let pikachu:any = {};
+
+const doFetch = async() => {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+    pikachu = await response.json();
+}
+doFetch();
 
 app.get('/',(req:any,res:any)=>{
     res.render("index");
@@ -24,14 +33,14 @@ app.get('/whoamijson',(req:any,res:any)=>{
 });
 
 app.get('/pikachujson',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.json(pikachu);
 });
 
 app.get('/pikachuhtml',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.render('pikachu', {pikachu: pikachu});
 });
 
 app.listen(app.get('port'), 
     ()=>console.log( '[server] http://localhost:' + app.get('port')));
+
+export {};
