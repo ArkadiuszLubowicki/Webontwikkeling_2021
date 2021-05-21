60976a99a284349d580f13cbe44362066ccea29f
Arkadiusz Lubowicki <s106960@ap.be>
2021-05-21 23:00:25 +0200

    3 Find movies

--- Labo10/index.ts
+++ Labo10/index.ts
@@ -25,8 +25,50 @@ const doDBCalls = async() =>{
     try{
 
         await client.connect();
+        console.log("-------------");
+        console.log("FIRST MOVIE");
+        console.log("-------------")
+        let firstMovie = await client.db(DATABASE).collection(COLLECTION).findOne({});
+        console.log(firstMovie);
 
-        
+        console.log("-------------");
+        console.log("SCORES");
+        console.log("-------------")
+        let cursor = await client.db(DATABASE).collection(COLLECTION).find({});
+        let allMovies = await cursor.toArray();
+        allMovies.forEach((m:Movie)=>{
+            console.log(m.myScore);
+        })
+
+        console.log("-------------");
+        console.log("ALL TIMES VIEWED");
+        console.log("-------------")
+        let timesViewedTotal = 0;
+        allMovies.forEach((m:Movie)=>{
+            timesViewedTotal += m.timesViewed;
+        })
+        console.log(timesViewedTotal);
+
+        console.log("-------------");
+        console.log("30 - 85");
+        console.log("-------------")
+        cursor = await client.db(DATABASE).collection(COLLECTION).find({
+            myScore:{$gt:30,$lt:85}});
+        let moviesBetween30_85 = await cursor.toArray();
+        moviesBetween30_85.forEach((m:Movie)=>{
+            console.log(m.name, m.myScore);
+        })
+
+        console.log("-------------");
+        console.log("30 - 85 OR ATLEAST VIEWED ONCE");
+        console.log("-------------")
+        cursor = await client.db(DATABASE).collection(COLLECTION).find({
+            $or:[{myScore:{$gt:30,$lt:85}},{timesViewed:1}]
+        });
+        let moviesBetween30_85ORViewedOnce = await cursor.toArray();
+        moviesBetween30_85ORViewedOnce.forEach((m:Movie)=>{
+            console.log(m.name, m.myScore, m.timesViewed);
+        })
 
 
         //let list = await client.db().admin().listDatabases();
@@ -35,9 +77,10 @@ const doDBCalls = async() =>{
         //await client.db(DATABASE).collection(COLLECTION).insertMany(movies);
         //await client.db(DATABASE).collection(COLLECTION).deleteMany({});
 
-        let cursor = await client.db(DATABASE).collection(COLLECTION).find({});
-        let result = await cursor.toArray();
-        console.log(result);
+        //PRINT ENTIRE DB
+        //let cursor = await client.db(DATABASE).collection(COLLECTION).find({});
+        //let result = await cursor.toArray();
+        //console.log(result);
     }
     catch(exc){
         console.log(exc);
